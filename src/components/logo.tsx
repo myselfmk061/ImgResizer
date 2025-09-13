142f603bc1df41c27c8d95d09ba979614c4f0cf4
Myselfmk Apps (A Development Company) <myselfmk061@users.noreply.github.com>
2025-09-13 06:44:33 +0000

    https://i.imghippo.com/files/oRi4335cO.png

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
+import Image from 'next/image';
 
 export function Logo() {
   return (
-    <a href="/" className="flex items-center gap-3">
-      <svg
-        width="32"
-        height="32"
-        viewBox="0 0 32 32"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect width="32" height="32" rx="6" fill="#F7F7F7" />
-        <path
-          d="M10 9H13V23H10V9Z"
-          fill="#F2762A"
-        />
-        <path
-          d="M13 9H18.554C20.9925 9 22.1263 10.2222 22.1263 12.5C22.1263 14.7778 20.9925 16 18.554 16H13V9Z"
-          fill="#DD5828"
-        />
-        <path
-          d="M13 16H18.5L22 23H18L15.5 17.5H13V16Z"
-          fill="#DD5828"
-        />
-      </svg>
+    <a href="/" className="flex items-center gap-2">
+      <Image
+        src="https://i.imghippo.com/files/oRi4335cO.png"
+        alt="ImgResizer Logo"
+        width={32}
+        height={32}
+        className="rounded-md"
+      />
       <span className="text-xl font-semibold">ImgResizer</span>
     </a>
   );
